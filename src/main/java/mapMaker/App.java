3f81be19bf59535b59f2440d6aef021ec4940221
JedrzejBronislaw <jedkru@wp.pl>
2019-11-22 23:48:10 +0100

    add first window

--- src/main/java/mapMaker/App.java
+++ src/main/java/mapMaker/App.java
@@ -1,8 +1,54 @@
 package mapMaker;
 
-public class App {
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application{
 	
 	public static void main(String[] args) {
 		System.out.println("Map Maker");
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("MapMaker");
+		stage.setScene(createScene());
+		stage.setOnCloseRequest(h -> Platform.exit());
+		
+		stage.show();
+	}
+
+	private Scene createScene() {
+		Pane pane = loadMainWindow();
+		
+		if (pane == null)
+			pane = new Pane();
+		
+		return new Scene(pane,600, 400);
+	}
+
+	private Pane loadMainWindow() {
+		FXMLLoader loader = new FXMLLoader();
+
+		loader.setLocation(getClass().getResource("view/MainWindow.fxml"));
+		
+		Pane pane;
+		
+		try {
+			pane = loader.load();
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		
+		return pane;
 	}
 }
---
+++ src/main/java/mapMaker/controllers/MainWindowController.java
@@ -0,0 +1,15 @@
+package mapMaker.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class MainWindowController implements Initializable{
+
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
