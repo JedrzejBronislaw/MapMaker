22b25fc03028f2229cfda8f5f2b2e546875a7928
JedrzejBronislaw <jedkru@wp.pl>
2019-11-23 23:59:51 +0100

    first view draft

--- src/main/java/mapMaker/controllers/MainWindowController.java
+++ src/main/java/mapMaker/controllers/MainWindowController.java
@@ -3,12 +3,20 @@ package mapMaker.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 public class MainWindowController implements Initializable{
 
+	@FXML
+	private Button generateButton;
+	
+	private void generate() {
+		
+	}
+	
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
 		
 	}
 
