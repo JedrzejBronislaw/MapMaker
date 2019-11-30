adc886f31228897a1b3f67c14dbbc78039710c2a
JedrzejBronislaw <jedkru@wp.pl>
2019-11-30 08:55:49 +0100

    change default properties for initial state generator

--- src/main/java/mapMaker/generators/caMapGenerator/InitialStateGenerators/CircleISGenOptions.java
+++ src/main/java/mapMaker/generators/caMapGenerator/InitialStateGenerators/CircleISGenOptions.java
@@ -6,6 +6,6 @@ public class CircleISGenOptions implements Options {
 	
 	public float centreX = 0.5f;
 	public float centreY = 0.5f;
-	public int centreR = 100;
-	public int circleR = 150;
+	public int centreR = 50;
+	public int circleR = 200;
 }
--- src/main/java/mapMaker/generators/caMapGenerator/InitialStateGenerators/TilesISGenOptions.java
+++ src/main/java/mapMaker/generators/caMapGenerator/InitialStateGenerators/TilesISGenOptions.java
@@ -6,6 +6,6 @@ public class TilesISGenOptions implements Options {
 
 	public int tilesX = 10;
 	public int tilesY = 10;
-	public float highProbability = 0.7f;
-	public float lowProbability = 0.3f;
+	public float highProbability = 0.8f;
+	public float lowProbability = 0.4f;
 }
